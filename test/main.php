c4bdd5901dda99c1273d97a65fcba3f522c37d2a
amralaa10 <3amr.3laa@gmail.com>
2017-12-26 19:49:16 +0200

    initial files provided by php-cpp lib, files are compiled and working

---
+++ test/main.php
@@ -0,0 +1,18 @@
+<?php
+    // define a user space function
+    function some_function($param)
+    {
+        echo("userspace function called with $param\n");
+    }
+
+    // example input
+    $input = array(
+        'x' =>  10,
+        'y' =>  20,
+        'z' =>  30
+    );
+
+    example_function($input, function($param1, $param2) {
+        echo("lambda function called with param $param1 $param2\n");
+    });
+?>
